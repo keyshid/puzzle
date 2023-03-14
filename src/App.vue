3644f97dd8ac1ae0f04bfab94e1579aff6665fb1
keyshid <keyshids@gmail.com>
2023-03-14 15:35:22 +0330

    puzzle done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="home">
+      <table>
+        <tbody>
+          <tr v-for="(items, row) in board" :key="row">
+            <td
+              v-for="(item, col) in items"
+              :key="col"
+              @click="swapBoard(row, col)"
+            >
+              {{ item }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <router-view/>
   </div>
 </template>
-
+<script>
+import {
+  createBoard,
+  mixBoard,
+  isSwappable,
+  getSwappableIndexes,
+  swap,
+  isCorretBoard,
+} from "../src/helper";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      size: 4,
+      board: [],
+      childBoard: [],
+    };
+  },
+  methods: {
+    swapBoard(items, item) {
+      if (isSwappable(this.board, [items, item])) {
+        const indexes = getSwappableIndexes(this.board, [items, item]);
+        this.board = swap(this.board, indexes[0], indexes[1]);
+      }
+      if (isCorretBoard(this.board)) {
+        alert("u win .");
+      }
+    },
+  },
+  created() {
+    this.board = createBoard(this.size);
+    this.board = mixBoard(this.board, 1000);
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100vh;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  td {
+    width: 100px;
+    height: 100px;
+    font-size: 40px;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    border: 2px solid black;
+    text-align: center;
+    -webkit-user-select: none;
+    user-select: none;
+    transition: 1s ease-in-out;
+  }
+  :hover {
+    cursor: pointer;
   }
 }
 </style>
